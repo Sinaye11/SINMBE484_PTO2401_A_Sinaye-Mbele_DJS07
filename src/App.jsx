d23ae3a72a025f433ab5ce6200a99db5cf26a1fc
Sinaye <sinaye.mbele@gmail.com>
2024-10-06 20:49:24 +0200

    challenge attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Header from "./components/Header.jsx"
+import Meme from "./components/Meme.jsx"
 
 function App() {
 
   return (
     <>
-     
+        <Header />
+        <Meme />
     </>
   )
 }
 
 export default App
+
